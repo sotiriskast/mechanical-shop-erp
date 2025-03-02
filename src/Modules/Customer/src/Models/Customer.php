ef9bae2a66c48066e7f778894bcc2ee9ccb6310a
sotiriskast <soteris100@gmail.com>
2025-03-02 13:22:40 +0200

    dev

--- src/Modules/Customer/src/Models/Customer.php
+++ src/Modules/Customer/src/Models/Customer.php
@@ -6,7 +6,6 @@ namespace Modules\Customer\src\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Support\Str;
 use Modules\Core\src\Traits\LogsActivity;
 use Spatie\MediaLibrary\HasMedia;
